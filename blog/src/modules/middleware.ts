46728c8a6ed30d03ef77f25d7f0fe98a3dfc3161
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2024-08-24 18:39:51 +0100

    feat: protect middleware

--- blog/src/modules/middleware.ts
+++ blog/src/modules/middleware.ts
@@ -1,4 +1,5 @@
 import { validationResult } from 'express-validator'
+import jwt from 'jsonwebtoken'
 
 export const validatePayload = function (req, res, next) {
   const result = validationResult(req)
@@ -9,4 +10,24 @@ export const validatePayload = function (req, res, next) {
     return
   }
   next()
+}
+
+export const protect = async function (req, res, next) {
+  const bearerToken = req.headers.authorization
+
+  if (!bearerToken?.startsWith('Bearer')) {
+    res.status(400).json({ message: 'Invalid bearer token' })
+    return
+  }
+
+  const token = bearerToken.split(' ')[1]
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET)
+    req.user = user
+    next()
+  } catch (err) {
+    console.log(err)
+    res.status(400).json({ message: 'Invalid bearer token' })
+  }
 }
